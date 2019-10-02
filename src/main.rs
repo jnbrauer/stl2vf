377b60cc726461ea273d4c407627ee9bb8fb254b
jnbrauer <jnbrauer@outlook.com>
2019-10-01 22:56:39 -0700

    Added Readme

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate stl2vf;
 use stl2vf::Mesh;
 
 fn main() {
-    let mesh = Mesh::from_stl("end1x10.stl").expect("Error converting STL");
+    let mesh = Mesh::from_stl("input.stl").expect("Error converting STL");
     println!("Mesh loaded");
     let model = mesh.voxelize().expect("Error voxelizing model");
     println!("Model voxelized");
